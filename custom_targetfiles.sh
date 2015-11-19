fd98fcfc06fcc8701e97837afebf3d188edaec23
叶柳 <yeliu@meizu.com>
2015-11-19 21:18:23 +0800

    Fixed the problem about couldn't recovery app and symlink to system

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -3,25 +3,48 @@
 MERGED_DIR=out/merged_target_files
 SYSTEM_DIR=$MERGED_DIR/SYSTEM
 DATA_DIR=$MERGED_DIR/DATA
-FLYME_DIR=$DATA_DIR/media/app
+FLYME_DIR=$DATA_DIR/media
+DATA_CONF=overlay/system/etc/data.conf
 
 rm -rf $DATA_DIR
-mkdir -p $FLYME_DIR
+mkdir -p $FLYME_DIR/fonts
+mkdir -p $FLYME_DIR/app
 
 function appRemoveToData ()
 {
-	for file in `grep -rns "board_remove_apps_to_data" Makefile | cut -d "=" -f2`
+	for file in `grep -rns "app2data" $DATA_CONF | cut -d "=" -f2`
 	do
 	    if [ -d "$SYSTEM_DIR/app/$file" ];then
-		echo "Moving $file from SYSTEM/APP to DATA"
-		mv $SYSTEM_DIR/app/$file $FLYME_DIR/
+		echo "Moving $file from SYSTEM/APP to DATA..."
+		mv $SYSTEM_DIR/app/$file $FLYME_DIR/app/
 	    fi
+	done
+}
+
+function privAppRemoveToData ()
+{
+	for file in `grep -rns "priv_app2data" $DATA_CONF | cut -d "=" -f2`
+	do
 	    if [ -d "$SYSTEM_DIR/priv-app/$file" ];then
-		echo "Moving $file from SYSTEM/PRIV-APP TO DATA"
-		mv $SYSTEM_DIR/priv-app/$file $FLYME_DIR/
+		echo "Moving $file from SYSTEM/PRIV-APP to DATA..."
+		mv $SYSTEM_DIR/priv-app/$file $FLYME_DIR/app/
+	    fi
+	done
+}
+
+function fontRemoveToData ()
+{
+	for file in `grep -rns "fonts2data" $DATA_CONF | cut -d "=" -f2`
+	do
+	    if [ -f "$SYSTEM_DIR/fonts/$file" ];then
+		echo "Moving $file from SYSTEM/FONTS to DATA..."
+		mv $SYSTEM_DIR/fonts/$file $FLYME_DIR/fonts/
 	    fi
 	done
 }
 
 appRemoveToData;
-cp -rf $FLYME_DIR $DATA_DIR
+privAppRemoveToData;
+fontRemoveToData;
+
+cp -rf $FLYME_DIR/app $DATA_DIR/
