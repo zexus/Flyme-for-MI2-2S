e0f2d7db6ef52b58f659b2758ad691a2ae301e3d
叶柳 <SiFooYee@gmail.com>
2016-06-14 10:39:42 +0800

    Adjust the position about integrate package for easy operation

--- build_integrate.sh
+++ build_integrate.sh
@@ -4,7 +4,7 @@ source ../../build/envsetup.sh
 
 cmi2s
 
-cp -rf integrate/system/app/ overlay/system/app/
+cp -rf ../integrate/system/app/ overlay/system/app/
 echo ">>> copying integrate/system/app/ to overlay/system/app/"
 
 make clean-all
